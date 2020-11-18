ec33758caf01d359b49454501e77f148d9782ec5
Ivan-Lebedev <ivieee@yandex.ru>
2020-11-18 19:02:38 +0400

    fix: add proxy to gh-pages

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://api-factory.simbirsoft1.com/api/',
+  baseURL: 'https://cors-anywhere.herokuapp.com/http://api-factory.simbirsoft1.com/api/',
   headers: {
     'X-Api-Factory-Application-Id': '5e25c641099b810b946c5d5b',
     Authorization: 'Bearer 4cbcea96de',
